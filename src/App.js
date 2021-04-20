b145f7a30f3aa1e239ef0e4969e47b5d0f81ddc6
pedro augusto <pedroaugustoinfo@gmail.com>
2021-04-20 15:58:49 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Button from "./button"
+
+function App() {
+  return (
+    <>
+    <button>button 1</button>
+    <button>button 2</button>
+    <button>button 3</button>
+    <button>button 4</button>
+    </>
+  );
+}
+
+export default App;
---
+++ src/button.js
@@ -0,0 +1,9 @@
+export default function Button (props) {
+    return(
+        <>
+        <br>
+        <button>{props.children}</button>
+        </br>
+    </>
+    )
+}
